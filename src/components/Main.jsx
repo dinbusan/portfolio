d6f1467b1531f2a82f6d25b72b574f1a7499d505
dinbusan <deann.videtto@gmail.com>
2023-05-07 20:41:50 +0200

    updated file sources for font and deann image

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -13,7 +13,7 @@ const Main = () => {
         <div className="bg-amber-400 w-60 h-60 md:h-80 md:w-80 rounded-full">
           <img
             className="object-top h-full w-full object-cover rounded-full"
-            src="./public/assets/deann.png"
+            src="./assets/deann.png"
             alt=""
           />
         </div>
