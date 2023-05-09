c36ead76b49497f61e7227b4fc7c03f43134a821
dinbusan <deann.videtto@gmail.com>
2023-05-09 19:55:00 +0200

    updated my about me page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -11,38 +11,43 @@ const About = () => {
       exit={{ opacity: 0, x: -100 }}
       transition={{ duration: 0.3 }}
     >
-      <div className="flex max-h-750px overflow-y-auto justify-center">
-        <p className="p-5 text-white text-xl w-7/8 md:w-3/4 ">
-          Hi, my name is Deann and I am a web developer with USA and EU
-          citizenship currently based in Amsterdam, The Netherlands.
+      {/* <div className="flex-col lg:flex-row max-h-750px overflow-y-auto justify-center">
+        <div className="mx-5 md:w-1/3 md:mx-auto lg:w-1/3 lg:ml-20 lg:pt-5"><img className="" src="./assets/deann_peace.jpeg" alt="" /></div>
+        <p className=" md:mx-auto md:text-center lg:text-left lg:w-1/3 p-5 text-white"> */}
+
+      <div className="lg:flex max-h-750px overflow-y-auto ">
+        <div className="mx-5 md:mx-auto md:w-1/2 lg:w-1/3 lg:ml-20 lg:pt-5">
+          <img
+            className="object-cover"
+            src="./assets/deann_peace.jpeg"
+            alt=""
+          />
+        </div>
+        <p className="md:mx-auto md:text-center lg:mx-5 lg:text-left lg:w-3/4  text-white">
+          Hello, I'm Deann, a full-stack JavaScript developer based in
+          Amsterdam, the Netherlands. <br />
+          <br /> Originally from Iowa in the US, I spent several years teaching
+          English in Busan, South Korea, where I honed my communication and
+          interpersonal skills. Prior to that, I worked in customer service and
+          education in the United States. <br />
           <br />
+          Driven by my passion for technology, I transitioned to a career in web
+          development. I taught myself the basics of web development through
+          online resources and bootcamps before enrolling in an intensive
+          full-stack JavaScript bootcamp in Amsterdam. Since then, I have been
+          constantly learning and experimenting with new technologies and
+          approaches to development.
           <br />
-          After teaching in the USA and South Korea for the past 8 years, I took
-          the initiative to self-study and learn the basics of programming.
-          However, I soon realized that I needed more structured training to
-          further develop my skills. Determined to pursue a career in software
-          engineering, I made the difficult decision to end my education career
-          and move to the Netherlands to join an intensive full-stack JavaScript
-          bootcamp. This decision has proven to be one of the best I have made,
-          as it allowed me to gain hands-on experience in developing complex web
-          applications using modern technologies and best practices.
+          <br />I have a keen interest in front-end development and creating
+          visually appealing and responsive websites. I particularly enjoy
+          working with ReactJS and other modern front-end frameworks. <br />
           <br />
-          <br /> I am passionate about software development and find myself
-          constantly seeking new challenges and opportunities to learn and grow.
-          I am excited about the prospect of collaborating with teams to develop
-          cutting-edge solutions to real-world problems, and contributing to the
-          success of organisations through my technical expertise and innovative
-          ideas. <br />
+          When I'm not coding, I enjoy playing board games, exploring new
+          places, and catching up on the latest true-crime documentaries. <br />
           <br />
-          Overall, I am a highly motivated and dedicated individual with a
-          strong work ethic, and I am committed to continuously improving my
-          skills and knowledge as a software developer. I am excited about what
-          the future holds and am ready to take on new challenges in the field
-          of software development. <br />
-          <br />I have hands-on experience with a variety of technologies,
-          including: HTML5, CSS3, MongoDB, PostgreSQL, UX/UI design, Git, REST
-          APIs, CI/CD, test-driven development, JavaScript, TypeScript, Node.js,
-          React.js, and Express.js.
+          Thank you for visiting my personal portfolio website. If you have any
+          questions or would like to collaborate on a project, please feel free
+          to get in touch via my contact page!
         </p>
       </div>
     </motion.div>
