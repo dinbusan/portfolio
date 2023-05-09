fff8066c8a02a258240b3d989a9bf269493a0a1f
dinbusan <deann.videtto@gmail.com>
2023-05-10 00:22:34 +0200

    updated margin on main page

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -10,7 +10,7 @@ const Main = () => {
   exit={ { opacity: 0, x: -100 }}
   transition={ { duration: 0.3 }}
     >
-      <div className="select-none mt-20 lg:mt-30 mx-auto xl:mt-30 xl:ml-40 text-center xl:text-left text-white roddenberry bg-black bg-opacity-20 md:bg-opacity-40">
+      <div className="select-none mt-20 lg:mt-30 mx-auto xl:mt-40 xl:ml-40 text-center xl:text-left text-white roddenberry bg-black bg-opacity-20 md:bg-opacity-40">
         <h1 className="text-5xl sm:text-7xl md:text-8xl xl:text-9xl">Hi, I'm Deann.</h1>
         <h1 className="text-4xl mt-10 md:text-5xl xl:text-6xl">
           A fullStack JavaScript developer.
