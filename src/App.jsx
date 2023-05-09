e7e749c5bcdd114440ea9586b70182e40b8ac7cf
dinbusan <deann.videtto@gmail.com>
2023-05-09 12:56:21 +0200

    updated styling for mobile and lg screen background

--- src/App.jsx
+++ src/App.jsx
@@ -7,24 +7,22 @@ import "./styles.css";
 function App() {
   return (
     <div
-      style={{ position: "relative", height: "100vh", width: "100vw" }}
-      // className="relative overflow-hidden h-full w-full"
+      className="relative h-screen w-screen"
     >
       <video
-        className="hidden md:block absolute inset-0 h-full w-full object-cover z-0"
+        className="hidden md:block fixed inset-0 h-full w-full object-cover z-0"
         autoPlay
         loop
         muted
-        style={{ position: "fixed" }}
       >
         <source src="/assets/fade_in_out.mp4" type="video/mp4" />
       </video>
       <div className="md:hidden absolute inset-0">
         <img
-          className="h-full w-full object-cover"
+          className="fixed h-full w-full object-cover"
           src="./assets/stars_img2.jpg"
           alt=""
-          style={{ position: "fixed" }}
+          // style={{ position: "fixed" }}
         />
       </div>
 
