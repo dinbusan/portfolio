c3043407843ab20cbb97e43e87005e89b8146ee8
dinbusan <deann.videtto@gmail.com>
2023-05-11 00:01:27 +0200

    updated animation on contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -44,7 +44,7 @@ const Contact = () => {
       initial={{ opacity: 0, x: 100 }}
       animate={{ opacity: 1, x: 0 }}
       exit={{ opacity: 0, x: -100 }}
-      transition={{ duration: 0.3 }}
+      transition={{ delay: 0.2, duration: .8 }}
     >
       {isSuccessfullySubmitted ? (
         <div className="fixed  font-mono mt-20 mx-1 text-center text-white bg-black bg-opacity-50 tracking-wider">
