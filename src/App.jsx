96af26a1fd63f2928194f1a0238421a03a7d4bad
dinbusan <deann.videtto@gmail.com>
2023-06-04 18:26:29 +0200

    adding a loading screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,57 @@
+import { useEffect, useState } from "react";
 import AnimatedRoutes from "./components/AnimatedRoutes";
 import Header from "./components/Header";
 // import Footer from "./components/Footer";
 import "./styles.css";
+import ScaleLoader from "react-spinners/ScaleLoader";
 
 function App() {
-  return (
-    <div
-      className="relative h-screen w-screen"
-    >
-      <video
-        className="hidden md:block fixed inset-0 h-full w-full object-cover z-0"
-        autoPlay
-        loop
-        muted
-      >
-        <source src="/assets/fade_in_out.mp4" type="video/mp4" />
-      </video>
-      <div className="md:hidden absolute inset-0">
-        <img
-          className="fixed h-full w-full object-cover"
-          src="./assets/stars_img2.jpg"
-          alt=""
-          // style={{ position: "fixed" }}
-        />
-      </div>
+  const [loading, setLoading] = useState(false);
 
-      <div className="relative z-1 h-full w-full">
-        <Header className="relative flex items-center justify-center h-screen w-screen overflow-hidden" />
-        <div className="route--container">
-          <AnimatedRoutes />
-        </div>
-      </div>
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 1700);
+  }, []);
+
+  return (
+    <div className="relative bg-black h-screen w-screen flex justify-center items-center">
+      {loading ? (
+        <ScaleLoader
+          color="#fbbf24"
+          height={100}
+          margin={5}
+          radius={50}
+          width={7}
+        />
+      ) : (
+        <>
+          <video
+            className="hidden md:block fixed inset-0 h-full w-full object-cover z-0"
+            autoPlay
+            loop
+            muted
+          >
+            <source src="/assets/fade_in_out.mp4" type="video/mp4" />
+          </video>
+          <div className="md:hidden absolute inset-0">
+            <img
+              className="fixed h-full w-full object-cover"
+              src="./assets/stars_img2.jpg"
+              alt=""
+              // style={{ position: "fixed" }}
+            />
+          </div>
+
+          <div className="relative z-1 h-full w-full">
+            <Header className="relative flex items-center justify-center h-screen w-screen overflow-hidden" />
+            <div className="route--container">
+              <AnimatedRoutes />
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 }
